1caa1fa55f9b2489484b9d958a3080af1965687f
Tran Dinh Quan <36017704+quan3107@users.noreply.github.com>
2026-09-05 20:43:22 +0700

    test(notebooks): register stronger dropout screen

--- tests/test_notebook_scaffolds.py
+++ tests/test_notebook_scaffolds.py
@@ -69,6 +69,7 @@ def test_only_planned_notebook_names_are_present() -> None:
         "04x_task3_gender_narrow64_screen.ipynb",
         "04y_task3_gender_dropout_screen.ipynb",
         "04aa_task3_gender_dropout_darkening_screen.ipynb",
+        "04ab_task3_gender_stronger_dropout_screen.ipynb",
         "04z_task3_usage_two_stage_screen.ipynb",
         *TASK_SPECS,
     }
