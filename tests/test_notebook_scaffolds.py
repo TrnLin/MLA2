1233e4c99b5a318f9602073e0fb4de4d3e747dee
Tran Dinh Quan <36017704+quan3107@users.noreply.github.com>
2026-09-06 15:01:56 +0700

    test(notebooks): allow the stronger MixUp screen

--- tests/test_notebook_scaffolds.py
+++ tests/test_notebook_scaffolds.py
@@ -74,6 +74,7 @@ def test_only_planned_notebook_names_are_present() -> None:
         "04ad_task3_gender_name_truth_screen.ipynb",
         "04ae_task3_gender_group_weight_screen.ipynb",
         "04af_task3_gender_mixup_screen.ipynb",
+        "04ah_task3_gender_stronger_mixup_screen.ipynb",
         "04z_task3_usage_two_stage_screen.ipynb",
         *TASK_SPECS,
     }
