3118432ea67b6f64315413d8029e30a729685075
Tran Dinh Quan <36017704+quan3107@users.noreply.github.com>
2026-09-06 16:59:46 +0700

    test(notebooks): allow the epoch-25 SAM notebook

--- tests/test_notebook_scaffolds.py
+++ tests/test_notebook_scaffolds.py
@@ -76,6 +76,7 @@ def test_only_planned_notebook_names_are_present() -> None:
         "04af_task3_gender_mixup_screen.ipynb",
         "04ah_task3_gender_stronger_mixup_screen.ipynb",
         "04ai_task3_gender_sam_screen.ipynb",
+        "04aj_task3_gender_sam25_screen.ipynb",
         "04z_task3_usage_two_stage_screen.ipynb",
         *TASK_SPECS,
     }
